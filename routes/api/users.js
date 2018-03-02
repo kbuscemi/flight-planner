b4d6abaaf7aaa5cc39f365245bb7f1d59bdb13e6
Kaylee Buscemi <kaylee.buscemi@gmail.com>
2018-03-01 18:10:25 -0800

    Deploy

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/User');
-const usersCtrl = require('../../controllers/userController');
+const usersCtrl = require('./../../controllers/userController');
 
 /* ~~~~~ Public Routes ~~~~ */
 router.post('/signup', usersCtrl.signup);
